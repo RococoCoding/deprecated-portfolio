5934a72d81ebfbb98af61e512f8e3fe9b346ae10
Alice Chang <sinsofangels@gmail.com>
2020-10-31 01:57:13 -0400

    Add skills and about content and styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,33 @@ import styled from "styled-components";
 import Skills from "./Skills";
 
 const AboutStyled = styled.main`
-
+  display: flex;
+  justify-content: space-between;
+  margin:2% 3%;
+  .about{
+    width: 45%;
+  }
+  h2 {
+    font-size: 1.8rem;
+    margin: 3% 0;
+    font-weight: bold;
+  }
+  .about p {
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    margin-bottom: 3%;
+  }
 `
 
 export default function About(props) {
   return (
     <AboutStyled>
       <Skills />
-      <section className="About">
-        <p>About Me</p>
+      <section className="about">
+        <h2>About Me</h2>
+        <p>I'm Alice, a web dev student at Lambda School learning JS, React, Redux, Node and a little bit of Python.</p> 
+        <p>I've always been a computer nerd, but never really did much coding beyond a computer applications course in high school. I had been feeling a bit restless the past few years, which resulted in my taking some time off to go to Maine to learn how to build a wooden rowboat??? I didn't know anything about wood or boats or tools, but I'm the kind of person who enjoys diving into new things. What I realized from that experience was that I wanted to make things, but also that I would very, very much wanted to keep all my fingers. Thus, my renewed interest in coding. </p>
+        <p>The timing seemed right when the pandemic hit and it seemed like a pretty good idea to hole myself up in front of a computer for a few months. I was the kid who did logic puzzles for fun, so I'm enjoying it a lot! I love solving problems so I get really in debugging and I love being able to make functional apps that people can use and interact with.</p>
       </section>
     </AboutStyled>
   );
---
+++ src/components/Skill.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const SkillStyled = styled.div`
+  display: flex;
+  padding: 1%;
+  .skill-bar {
+    background-color: darkturquoise;
+    min-height: 8px;
+    border-top-right-radius: 3px;
+    border-bottom-right-radius: 3px;
+  }
+  .skill-text {
+    padding-right: 2%;
+    width: 24%;
+  }
+  .Regex {
+    background-color: red;
+    padding-left: 2%;
+    white-space: nowrap;
+  }
+`
+
+export default function Skill(props) {
+  return (
+    <SkillStyled>
+      <p className="skill-text">{props.language}</p>
+      <div className={`skill-bar ${props.language}`} style={{width: `${props.width}%`}}>{props.extra && props.extra}</div>
+    </SkillStyled>
+  )
+}
